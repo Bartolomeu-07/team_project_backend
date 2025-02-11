2a2fb85fc9dc2549a25a4a9d4d25301606342fad
Bartolomeu <bartoszokroj@wp.pl>
2025-02-11 16:37:53 +0100

    Static files

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,9 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL dla plików statycznych
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Katalog dla Render
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Jeśli masz dodatkowe pliki statyczne
+]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
