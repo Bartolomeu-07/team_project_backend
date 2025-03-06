18253b2e40aaa426026ee6917888e6321566f35d
Bartolomeu <bartoszokroj@wp.pl>
2025-03-06 15:42:26 +0100

    match model representation

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -12,16 +12,19 @@ class MatchSerializer(serializers.ModelSerializer):
         return {
             "competition": instance.competition.name if instance.competition else None,
             "match_info": {
-                "match_id": instance.id,
+                "match_id": instance.match_id,
                 "home_team": instance.home_team.name,
                 "away_team": instance.away_team.name,
                 "home_team_logo": instance.home_team.logo,
                 "away_team_logo": instance.away_team.logo,
                 "home_team_last_five_results": instance.home_team_last_five_results,
                 "away_team_last_five_results": instance.away_team_last_five_results,
+                "home_wins_probability": instance.home_wins_probability,
+                "away_wins_probability": instance.away_wins_probability,
                 "home_score": instance.home_score,
                 "away_score": instance.away_score,
                 "date_time": instance.date_and_time,
+                "status": instance.status,
             },
         }
 
