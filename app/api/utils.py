091f8eb41a7744219b184f2ea069cf646a6e12c8
Bartolomeu <bartoszokroj@wp.pl>
2025-04-23 20:42:07 +0200

    Calculate probabilities

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,9 +1,12 @@
 import random
-
 import requests
-from random import choices, uniform
+import joblib
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from django.db.models import Q
 
 from .models import Country, Competition, Match, Team
+from .logistic_regression import calculate_probabilities
 
 API_KEY = '0707a7f0e4872c2bfc2763c2b27ba7b4'
 HEADERS = {"x-apisports-key": API_KEY}
@@ -108,7 +111,7 @@ def fetch_and_create_teams():
 
 
 def create_or_update_match_data():
-    competitions_to_update = [135, 61, 78, 2, 140, 39]
+    competitions_to_update = [135, 2, 61, 78, 140, 39]
 
 
     for competition_id in competitions_to_update:
@@ -120,8 +123,8 @@ def create_or_update_match_data():
         response = requests.get(url + 'fixtures', headers=HEADERS, params=params)
         if response.status_code == 200:
             data = response.json().get('response', [])
-            existing_matches = Match.objects.filter(competition_id=params.get('league'))
-            existing_matches_ids = set(existing_matches.values_list('match_id', flat=True))
+            existing_matches = list(Match.objects.filter(competition_id=params.get('league')).order_by('-date_and_time'))
+            existing_matches_ids = set(m.match_id for m in existing_matches)
             competition = Competition.objects.get(competition_id=params.get('league'))
             teams = Team.objects.all()
             new_matches = []
@@ -133,34 +136,194 @@ def create_or_update_match_data():
                     away_team_id = item.get('teams').get('away').get('id')
                     home_team = teams.get(team_id=home_team_id)
                     away_team = teams.get(team_id=away_team_id)
+                    date = item.get('fixture').get('date')
+
+                    # Loading last 10 matches data for each team
+                    home_team_last_ten_matches = [
+                        match for match in existing_matches
+                        if (match.home_team == home_team or match.away_team == home_team)
+                           and match.status == 'Match Finished'
+                           and match.date_and_time < date
+                    ]
+                    home_team_last_ten_matches = sorted(home_team_last_ten_matches, key=lambda m: m.date_and_time,
+                                                        reverse=True)[:10]
+
+                    away_team_last_ten_matches = [
+                        match for match in existing_matches
+                        if (match.home_team == away_team or match.away_team == away_team)
+                           and match.status == 'Match Finished'
+                           and match.date_and_time < date
+                    ]
+                    away_team_last_ten_matches = sorted(away_team_last_ten_matches, key=lambda m: m.date_and_time,
+                                                        reverse=True)[:10]
+
                     home_team_last_five_results = random.choices(['W', 'D', 'L'], k=5)
                     away_team_last_five_results = random.choices(['W', 'D', 'L'], k=5)
                     home_score = item.get('goals').get('home')
                     away_score = item.get('goals').get('away')
-                    home_wins_probability = round(uniform(-1, 1), 2)
-                    away_wins_probability = round(uniform(-1, 1), 2)
-                    date = item.get('fixture').get('date')
+                    home_wins_probability, away_wins_probability = calculate_probabilities(
+                        home_team, away_team, home_team_last_ten_matches, away_team_last_ten_matches
+                    )
                     status = item.get('fixture').get('status').get('long')
 
-                    new_matches.append(Match(match_id=match_id,
-                                             competition=competition,
-                                             home_team=home_team,
-                                             away_team=away_team,
-                                             home_team_last_five_results=home_team_last_five_results,
-                                             away_team_last_five_results=away_team_last_five_results,
-                                             home_score=home_score,
-                                             away_score=away_score,
-                                             home_wins_probability=home_wins_probability,
-                                             away_wins_probability=away_wins_probability,
-                                             date_and_time=date,
-                                             status=status
-                                             ))
+                    new_match = Match(match_id=match_id,
+                                      competition=competition,
+                                      home_team=home_team,
+                                      away_team=away_team,
+                                      home_team_last_five_results=home_team_last_five_results,
+                                      away_team_last_five_results=away_team_last_five_results,
+                                      home_score=home_score,
+                                      away_score=away_score,
+                                      home_wins_probability=home_wins_probability,
+                                      away_wins_probability=away_wins_probability,
+                                      date_and_time=date,
+                                      status=status
+                                      )
+                    new_matches.append(new_match)
+                    existing_matches.append(new_match)
             if new_matches:
                 Match.objects.bulk_create(new_matches)
                 print(f"{len(new_matches)} new {competition.name} matches created.")
             else:
                 print(f"No new {competition.name} matches to add.")
-        else:
-            print("Error during fetching matches:", response.status_code)
 
 
+def fit_ai_models():
+    # Data fetching
+    data = {
+        # features
+        'home_avg_goals_scored': [],
+        'home_avg_goals_conceded': [],
+        'home_avg_points': [],
+        'away_avg_goals_scored': [],
+        'away_avg_goals_conceded': [],
+        'away_avg_points': [],
+        # labels
+        'home_win': [],
+        'away_win': [],
+    }
+
+    all_matches = Match.objects.all()
+    train_matches = all_matches.order_by('-date_and_time')[:100]
+
+    # Calculate features and labels for every single match from training set
+    for match in train_matches:
+        home_team = match.home_team
+        away_team = match.away_team
+        date = match.date_and_time
+
+        # Loading last 10 matches data for each team
+        home_team_last_ten_matches = all_matches.filter(
+            (Q(home_team=home_team) | Q(away_team=home_team))
+            & Q(status='Match Finished') & Q(date_and_time__lt=date)
+        ).order_by('-date_and_time')[:10]
+        away_team_last_ten_matches = all_matches.filter(
+            (Q(home_team=away_team) | Q(away_team=away_team))
+            & Q(status='Match Finished') & Q(date_and_time__lt=date)
+        ).order_by('-date_and_time')[:10]
+
+        # Skip a match if is not any history data
+        if len(home_team_last_ten_matches) == 0 or len(away_team_last_ten_matches) == 0:
+            continue
+
+        # Calculate goals AVGs
+        home_team_goals_scored = 0
+        home_team_goals_conceded = 0
+        away_team_goals_scored = 0
+        away_team_goals_conceded = 0
+
+        for m in home_team_last_ten_matches:
+            if m.home_team == home_team:
+                home_team_goals_scored += m.home_score
+                home_team_goals_conceded += m.away_score
+            else:
+                home_team_goals_scored += m.away_score
+                home_team_goals_conceded += m.home_score
+        home_team_goals_scored_avg = home_team_goals_scored / len(home_team_last_ten_matches)
+        home_team_goals_conceded_avg = home_team_goals_conceded / len(home_team_last_ten_matches)
+
+        for m in away_team_last_ten_matches:
+            if m.home_team == away_team:
+                away_team_goals_scored += m.home_score
+                away_team_goals_conceded += m.away_score
+            else:
+                away_team_goals_scored += m.away_score
+                away_team_goals_conceded += m.home_score
+        away_team_goals_scored_avg = away_team_goals_scored / len(away_team_last_ten_matches)
+        away_team_goals_conceded_avg = away_team_goals_conceded / len(away_team_last_ten_matches)
+
+        # Calculate points AVGs
+        home_team_points = 0
+        for m in home_team_last_ten_matches:
+            if m.home_team == home_team:
+                if m.home_score > m.away_score:
+                    home_team_points += 3
+                elif m.home_score == m.away_score:
+                    home_team_points += 1
+            else:
+                if m.home_score < m.away_score:
+                    home_team_points += 3
+                elif m.home_score == m.away_score:
+                    home_team_points += 1
+        home_team_points_avg = home_team_points / len(home_team_last_ten_matches)
+
+        away_team_points = 0
+        for m in away_team_last_ten_matches:
+            if m.home_team == away_team:
+                if m.home_score > m.away_score:
+                    away_team_points += 3
+                elif m.home_score == m.away_score:
+                    away_team_points += 1
+            else:
+                if m.home_score < m.away_score:
+                    away_team_points += 3
+                elif m.home_score == m.away_score:
+                    away_team_points += 1
+        away_team_points_avg = away_team_points / len(away_team_last_ten_matches)
+
+        # Append data to data-features
+        data['home_avg_goals_scored'].append(home_team_goals_scored_avg)
+        data['away_avg_goals_scored'].append(away_team_goals_scored_avg)
+        data['home_avg_goals_conceded'].append(home_team_goals_conceded_avg)
+        data['away_avg_goals_conceded'].append(away_team_goals_conceded_avg)
+        data['home_avg_points'].append(home_team_points_avg)
+        data['away_avg_points'].append(away_team_points_avg)
+        data['home_win'].append(match.home_score > match.away_score)
+        data['away_win'].append(match.away_score > match.home_score)
+
+    # Create DataFrame
+    df = pd.DataFrame(data)
+
+    # DataFrame features
+    features = [
+        'home_avg_goals_scored',
+        'home_avg_goals_conceded',
+        'home_avg_points',
+        'away_avg_goals_scored',
+        'away_avg_goals_conceded',
+        'away_avg_points'
+    ]
+
+    # Input data
+    X = df[features].values
+
+    # Labels
+    y_home = df['home_win'].values
+    y_away = df['away_win'].values
+
+    # Regression model for home team
+    model_home = LogisticRegression()
+    model_home.fit(X, y_home)
+    pred_home_win = model_home.predict_proba(X)[:, 1]
+
+    # -||- away team
+    model_away = LogisticRegression()
+    model_away.fit(X, y_away)
+    pred_away_win = model_away.predict_proba(X)[:, 1]
+
+    print("Prawdopodobieństwo zwycięstwa gospodarzy:", pred_home_win)
+    print("Prawdopodobieństwo zwycięstwa gości:", pred_away_win)
+
+    # Saving models to pickle files
+    joblib.dump(model_home, 'model_home_wins.pkl')
+    joblib.dump(model_away, 'model_away_wins.pkl')
